810fa48beb20e977f14a2341ff4f03c2d4097a81
Javo-Martinez <117385106+Javo-Martinez@users.noreply.github.com>
2026-08-24 15:16:02 -0300

    Add files via upload

---
+++ revista28/content/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, user-scalable=no">
+<meta http-equiv="Expires" content="0">
+<meta http-equiv="Last-Modified" content="0">
+<meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+<meta http-equiv="Pragma" content="no-cache">
+<meta provider="DonWeb" content="no-cache">
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-3454246-8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-3454246-8');
+</script>
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+
+<title>.:: ESTO NO ES UNA REVISTA  ::. </title>
+
+<link rel="stylesheet" type="text/css" href="css/index.css">
+
+</head>
+
+<body>
+
+<div id="base">
+
+	<img class="imageIndex" src="images/index.jpg" alt="" />
+    
+    <p class="poemaIndex">
+    <br />
+    De tus uvas degusto el secreto <br />
+    Guardado el tiempo preciso <br />
+    Misterios de mezclado sabor <br />
+    Aromas entreverados, uno y otro <br />
+    Translúcido, rojo sangre<br />
+    Embebernos contigo, él, ella,<br />
+    vos y yo
+
+    </p>
+    
+    
+    
+    <p class ="entrar copyIndex">
+    
+    	<a href="eneur.php?pag=0">Entrar
+        </a>
+    
+    <p>
+
+
+</div>
+
+</body>
+
+</html>
