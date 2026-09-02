5f816ea1a62f0ede42776991faed92b7c4ebdae8
Javo-Martinez <117385106+Javo-Martinez@users.noreply.github.com>
2026-09-02 19:26:51 -0300

    Update index.php #39

--- revista39/content/index.php
+++ revista39/content/index.php
@@ -14,20 +14,10 @@
 
 <body>
 
-	<div id="base_">
 
-		<div class="base_log">
-            <a href="eneur.php?pag=0">
-        		<img src="images/logo_index.jpg" alt=""  />
-            </a>
-        </div>
-
-	<div class="base_txt">
-
-		<!--SETEO EL NOMBRE DEL NUMERO Y LA INTRO-->
 
       	<div class="titul_ag">La lluvia</div>
-      	<div class="txt_intro">
+      	<div class="poemaIndex">
       	  <p>Finita la lluvia cay&oacute; esta ma&ntilde;ana, <br />
       	    que llueva, que llueva&hellip;<br />
       	    La otra tarde v&iacute; llover, <br />
@@ -40,64 +30,9 @@
       	    palabras, agujeros y silencios<br />
       	    y el litoral precipitado movi&eacute;ndose<br />
    	      nuevos contornos, hasta una nueva vez.</p>
-</div>
-      	<div class="menu_intro">
+		</div>
+
       	  
-      	  <!--            
-				<div id="int"> 
-            	<a href="indice.html" class="link3"> INDICE</a>
-            	<div class="footer_s">
-                I
-                </div> 
-                <a href="sonoridades.html" class="link3"> SONORIDADES</a>
-                <div class="footer_s">
-                I
-                </div>
-                <a href="escritos.html" class="link3">ESCRITOS</a>
-                <div class="footer_s">
-                I
-                </div>
-                <a href="miradas.html" class="link3"> MIRADAS</a>
-                <div class="footer_s">
-                I
-                </div>
-                <a href="sabores.html" class="link3"> SABORES</a>
-                <div class="footer_s">
-                I
-                </div>
-                <a href="blablabla.html" class="link3"> BLA BLA BL&Aacute;</a> 
-                
-                <div class="footer_s">
-                I
-                </div>
-
-                
-			</div>}
--->
-      	  
-<div id="pie">
-                <strong>ESTO NO </strong>ES UNA REVISTA ¬ cultura digital ¬ <strong>ISSN 1853-5194</strong> ¬  <strong>&copy;</strong> 2010 Andrea Barone &amp; Javier Mart&iacute;nez
-          </div>
-</div>
-        	
-        <div class="entrar">
-        	<a href="eneur.php?pag=0">ENTRAR</a>
-        </div>
-        
-                              
-    
-
-    </div>
-
-  
-
-  </div>
-
-<!--<div class="footer_intro">esto no es una revista ¬ <span class="footer_cultura">cultura digital</span> <span class="arti">¬ ISSN 1853-5194 </span><span class="arti">¬  &copy; 2010 Andrea Barone &amp; Javier Mart&iacute;nez</span></div>-->
-
-
-</div>
-
 </body>
 
 </html>
