cb72a0675a8df9516d7b74edf4eb76030baf1993
Javo-Martinez <117385106+Javo-Martinez@users.noreply.github.com>
2026-09-09 17:51:04 -0300

    Update 18.php
    cambio id="gallery" por class="gallery"

--- revista39/content/18.php
+++ revista39/content/18.php
@@ -11,7 +11,7 @@ Selección de Alejandro Feijóo.
     </strong>
 </p>
 
-<div id="gallery" data-gallery="maier-autorretratos"></div>
+<div class="gallery" data-gallery="maier-autorretratos"></div>
 
 <p>
 	<strong>
@@ -19,35 +19,35 @@ Selección de Alejandro Feijóo.
     </strong>
 </p>
 
-<div id="gallery" data-gallery="maier-chicos"></div>
+<div class="gallery" data-gallery="maier-chicos"></div>
 
 <p>
 	<strong>
     	Ciudad
     </strong>
 </p>
-<div id="gallery" data-gallery="maier-ciudad"></div>	
+<div class="gallery" data-gallery="maier-ciudad"></div>	
 <p>
 	<strong>
     	Hombres
     </strong>
 </p>
-<div id="gallery" data-gallery="maier-hombres"></div>
+<div class="gallery" data-gallery="maier-hombres"></div>
 <p>
 	<strong>
     	Mujeres
     </strong>
 </p>
-<div id="gallery" data-gallery="maier-mujeres"></div>
+<div class="gallery" data-gallery="maier-mujeres"></div>
 <p>
 	<strong>
     	Naturaleza muerta
     </strong>
 </p>
-<div id="gallery" data-gallery="maier-naturalezamuerta"></div>
+<div class="gallery" data-gallery="maier-naturalezamuerta"></div>
 <p>
 	<strong>
     	Parejas
     </strong>
 </p>
-<div id="gallery" data-gallery="maier-parejas"></div>
+<div class="gallery" data-gallery="maier-parejas"></div>
