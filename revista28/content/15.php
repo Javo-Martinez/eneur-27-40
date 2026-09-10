b11b060964d2386ce1aa39971b09b93aebdae645
Javo-Martinez <117385106+Javo-Martinez@users.noreply.github.com>
2026-09-10 17:52:48 -0300

    Update 15.php

--- revista28/content/15.php
+++ revista28/content/15.php
@@ -7,44 +7,9 @@
 Víctor Mahana (Santiago de Chile, 1977).  Licenciado en Arte. Pintor, grabador e ilustrador. Su obra forma parte de  colecciones chilenas e internacionales. Ha realizado más de medio centenar de  exposiciones colectivas e individuales, entre las que destaca &ldquo;Cuando desperté  no había nadie&rdquo;, recientemente clausurada en el Museo Nacional de Bellas Artes  de Chile. <br />
   <a href="http://www.victormahana.com/">www.victormahana.com</a></p>
 </span>
-<br />
+<div id="gallery" data-gallery="pinturas"></div>
 
-<? 
-	//RECUPERO LA ESTRUCTURA DE LA PAGINA
-	$imagen = 	"SELECT 
-				cantidadImagenes
-				FROM notas
-				WHERE idPagina = ". $pag . "
-				AND edicionNumero =". $numeroActual; 
-	
-
-	
-			echo "<div id=\"slider2\">";
-			
-				$paginaimagen = mysql_query($imagen) or die("Couldn't execute query: ".$imagen);
-				
-				while ($row= mysql_fetch_array($paginaimagen)) 
-				{
-					$cantidadImagenes 	= $row["cantidadImagenes"];
-				}
-	
-				for ($i=1; $i<=$cantidadImagenes; $i++)
-				{
-					$size = GetImageSize("pinturas/".$i.".jpg");	
-					$anchura=$size[0];
-					$nuevoAncho = (770 - $anchura) / 2;
-					echo "<img id=".$i." src=\"pinturas/".$i.".jpg\" style=\"margin-left:".$nuevoAncho."px\" />";	
-				}
-				
-			
-				echo "<div id=\"titulo\">";
-//								
-//					for ($i=1; $i<=$cantidadImagenes; $i++)
-//					{
-//						echo"<p> Titulo ".($i). "</p>";
-//					}
-				?>	
-                	<tt>
+<!--
 					<p> Babel (2013). Óleo sobre tela (200 x 160 cm)</p>
 					<p> Caída libre (2013). Óleo sobre tela (120 x 90 cm) </p>
 					<p> Camino (2013). Óleo sobre tela (120 x 90 cm) </p>
@@ -61,73 +26,4 @@ Víctor Mahana (Santiago de Chile, 1977).  Licenciado en Arte. Pintor, grabador 
                     <p> Cuando desperté no había nadie (Tríptico) (2013). <br />
 Óleo sobre tela (medidas totales: 560 x 200 cm) </p>
                     <p> La torre (2012). Óleo sobre tela (200 x 160 cm) </p>
-                    </tt>
-                <?
-//				
-				echo "</div>";			
-			
-			echo "</div>";
-
-			echo "<div id=\"contenedorGaleria\">";
-			
-				echo "<div id=\"galeriaTexto\" onclick=\"slider2('A')\"> <tt>'<'Anterior</tt>  </div>";
-
-	
-				echo "<div id=\"galeriaAnt\">";
-				
-					for ($i=1; $i<=$cantidadImagenes; $i++)
-					{
-						if (($i-1)==0)
-						{	
-							$iA = $cantidadImagenes;
-						}
-						else
-						{
-							$iA = $i-1;
-						}
-						echo"<img style=\"float:left\" src=\"pinturas/mini".($iA).".jpg\" />";
-					}
-	
-				echo "</div>";
-
-
-				echo "<div id=\"galeria\">";
-				
-					for ($i=1; $i<=$cantidadImagenes; $i++)
-					{
-						echo"<img style=\"float:left\" src=\"pinturas/mini".($i).".jpg\"/>";
-					}
-	
-				echo "</div>";
-				
-				
-			
-				echo "<div id=\"galeriaSig\">";
-				
-					for ($i=1; $i<=$cantidadImagenes; $i++)
-					{
-						
-						if (($i+1) > $cantidadImagenes)
-						{	
-							$iS = 1;
-						}
-						else
-						{
-							$iS = $i+1;
-						}
-						
-						echo"<img style=\"float:left\" src=\"pinturas/mini".($iS).".jpg\" />";
-					}
-	
-				echo "</div>";
-			
-				  
-				echo "<div id=\"galeriaTexto\" onclick=\"slider2('P')\"> <tt>Siguiente'>'</tt> </div>";	
-				
-
-
-			
-			echo "</div>";
-			
-
-?>
+-->
